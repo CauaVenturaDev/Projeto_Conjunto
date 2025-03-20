e11489b534a385096f2e2f1f5e9bff68ee3b6416
Marcos Eduardo <marcoseduardodas.carvalho@hotmail.com>
2025-03-20 18:38:19 -0300

    2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('Hello, world!');
+class user {
+    constructor(name, password) {
+        this.name = name;
+        this.password = password;
+    }
+}
+const user1 = new user('Teste', '123456');
+
+const divs = [];
+for (let i = 0; i < 5; i++) {
+    const div = document.createElement('div');
+    div.innerText = `Div ${i}`;
+    divs.push(div);
+}
+
+
+divs.map(div => 
+    document.body.classList.add('container'),
+    document.body.appendChild(div)
+);
+
+const root = document.getElementById('root');
+
+
+root.innerText = 'Hello, World!';
+
